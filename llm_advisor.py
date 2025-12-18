f19575e3e1f3a2a2ac65575083a76412e5d041a7
Sasanka Rajapaksha <121901716+Saza-dev@users.noreply.github.com>
2025-12-18 19:03:18 +0530

    Change import of RetrievalQA to community version

--- llm_advisor.py
+++ llm_advisor.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 import re
 
 load_dotenv()
